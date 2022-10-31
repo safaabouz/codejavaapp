dd5ec45e36f4fb8d1fa2e7179aebb34624db2b7b
safaabouz <hp@DESKTOP-BOF3PFR>
2022-10-31 18:37:14 +0200

    bb

--- src/main/java/clinic/Invoice.java
+++ src/main/java/clinic/Invoice.java
@@ -1,8 +1,19 @@
 package clinic;
 
 public class Invoice {
-	int count;
+	int count=0;
 	String doctor_cost;
 	String med_cost;
+	
+	
+	public Invoice(String doctorcost,String medcost){
+		
+		doctor_cost=doctorcost;
+		med_cost=medcost;
+		
+	}
+	public Invoice() {
+		// TODO Auto-generated constructor stub
+	}
 
 }
